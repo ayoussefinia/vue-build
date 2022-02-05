f969edee62aa03dc488de9ebb557e42e857c10c7
Alex Youssefinia <alex.youssefinia@gmail.com>
2022-02-05 11:08:39 -0600

    started moving static project files into dynamic spa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <header class="top-bar spread">
+        <nav class="top-bar-nav">
+          <router-link> to="/" class="top-bar-link">
+            <i class="icofont-spoon-and-fork"></i>
+            <span>Home</span>
+          </router-link>
+          <router-link> to="/products" class="top-bar-link">
+            <span>Products</span>
+          </router-link>
+          <router-link> to="/past-orders" class="top-bar-link">
+            <span>Past Orders</span>
+          </router-link>
+        </nav>
+        <a @click="toggleSidebar" href="#" class="top-bar-cart-link">
+          <i class="icofont-cart-alt icofont-1x"></i>
+          <span>Cart ({{totalQuantity}})</span>
+        </a>
+      </header>
+  <router-view/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
